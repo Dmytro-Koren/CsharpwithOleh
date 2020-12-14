080ec447c2499ea828164e61eec69e5bc9edefd5
Dmytro-Koren <koren.dmytro@gmail.com>
2020-12-14 16:38:27 +0200

    Add completed Task_1

---
+++ Task_1(helloworld+currentdate)/Task_1(helloworld+currentdate)/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Task_1_helloworld_currentdate_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!" + DateTime.Now);
+            Console.ReadKey();
+        }
+    }
+}
